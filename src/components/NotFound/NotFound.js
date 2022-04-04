82d2c53f3b550f5a23c87adeeb8538396b3cd69c
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-04 20:24:46 +0600

    404 page edit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-           <img className='w-1/2 pt-20 mx-auto items-center' src="https://weeblytutorials.com/wp-content/uploads/2017/05/Weebly-404-Page-Not-Found-Error1.png" alt="" />
+           <img className='w-1/2 pt-20 mx-auto items-center' src="https://weeblytutorials.com/wp-content/uploads/2017/05/Weebly-404-Page-Not-Found-Error1.png" alt=" 404 Page Not-Found" />
         </div>
     );
 };
