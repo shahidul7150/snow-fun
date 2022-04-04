8339692c938d428412a91dbbca7b989438112ca7
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-04 12:12:52 +0600

    home page customer review ok

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import CustomerSay from '../customerSay/CustomerSay';
-import './Home.css'
+import React, { useEffect, useState } from "react";
+import CustomerSay from "../customerSay/CustomerSay";
+import "./Home.css";
 const Home = () => {
-    
-    return (
-        <div>
-            <div className='flex justify-around mt-8 items-center'>
-             <div className='text-5xl font-bold '>
-                <h2>It's Not Just snow-board</h2>
-                <h2 className='text-cyan-400'>Enjoy your life </h2>
-                <p className='text-xl font-normal
-                pt-4 text-gray-500 '>Snow-board is a tool that is use for ride in snow field . So Enjoy and fun your life.</p>
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("review.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <div>
+      <div className="flex justify-around mt-8 items-center">
+        <div className="text-5xl font-bold ">
+          <h2>It's Not Just snow-board</h2>
+          <h2 className="text-cyan-400">Enjoy your life </h2>
+          <p
+            className="text-xl font-normal
+                pt-4 text-gray-500 "
+          >
+            Snow-board is a tool that is use for ride in snow field . So Enjoy
+            and fun your life.
+          </p>
 
-                <button className='bg-cyan-400 text-xl py-2 px-5 font-semibold text-white rounded mt-8' >DEMO</button>
-            </div>
-            <img className='img-size' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-rF7WRmhRWhRx9GmNPb0x0lMNpqu3Z8qP9g&usqp=CAU" alt="" />
-           
-            </div>
-            <CustomerSay></CustomerSay>
+          <button className="bg-cyan-400 text-xl py-2 px-5 font-semibold text-white rounded mt-8">
+            DEMO
+          </button>
         </div>
-    );
+        <img
+          className="img-size"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-rF7WRmhRWhRx9GmNPb0x0lMNpqu3Z8qP9g&usqp=CAU"
+          alt=""
+        />
+      </div>
+      <h1 className="text-3xl font-semibold mt-20 text-orange-600">
+        Our Customers Say
+      </h1>
+      <div className="reviews-container">
+        {reviews.map((review) => (
+          <CustomerSay key={review.id} review={review}></CustomerSay>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/customerSay/CustomerSay.js
+++ src/components/customerSay/CustomerSay.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./CustomerSay.css";
+const CustomerSay = ({ review }) => {
+  console.log(review);
+  const { name, image, detail, ratting } = review;
 
-const CustomerSay = () => {
-    return (
-        <div>
-            <h1 className='text-3xl font-semibold mt-20'>Our Customers Say</h1>
-            
+  return (
+    <div className="review-container max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+      <div className="flex justify-center md:justify-end -mt-16">
+        <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src={image} alt="" />
+      </div>
+
+        <div >
+          <p className="text-gray-800 text-2xl font-semibold">{name}</p>
+          <p className="mt-2 text-gray-600">Ratting:★ ({ratting})</p>
         </div>
-    );
+        <div>
+          <p >{detail}</p>
+        </div>
+      
+    </div>
+  );
 };
 
 export default CustomerSay;
